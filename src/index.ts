e6bf112405ff545727aa977ad5ea20f4b1432f98
Julior <juliordajko@gmail.com>
2026-08-02 13:07:58 +0200

    implement GET all subjects route

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,27 @@
 import express from "express";
+import subjectsRouter from "./routes/subjects.js";
+import cors from "cors";
 
 const app = express();
 const PORT = 8000;
 
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL is not defined");
+}
+
+if (!process.env.FRONTEND_URL) {
+  throw new Error("FRONTEND_URL is not defined");
+}
+
+app.use(cors({
+  origin: process.env.FRONTEND_URL,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true,
+}));
+
 app.use(express.json());
+
+app.use('/api/subjects', subjectsRouter); // Use the subjects router for /api/subjects routes);
 
 app.get("/", (req, res) => {
   res.send("Welcome to the Classroom Backend!");
