8adc254e6576128b2bba7cee385a229fb557c3d5
Julior <juliordajko@gmail.com>
2026-08-08 21:10:40 +0200

    implement class details

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,43 @@
-import express from 'express';
+import('apminsight')
+  .then(({ default: AgentAPI }) => AgentAPI.config())
+  .catch(() => console.log('APM not available in this environment'));
+
 import cors from "cors";
+import express from "express";
+import { toNodeHandler } from "better-auth/node";
+
 import subjectsRouter from "./routes/subjects.js";
 import usersRouter from "./routes/users.js";
 import classesRouter from "./routes/classes.js";
-import securityMiddleware from "./middleware/security.js";
-import { toNodeHandler } from "better-auth/node";
+
+// import securityMiddleware from "./middleware/security.js";
 import { auth } from "./lib/auth.js";
 
 const app = express();
 const PORT = 8000;
 
-if (!process.env.FRONTEND_URL) throw new Error('FRONTEND_URL is not set in .env file');
-
-app.use(cors({
-    origin: process.env.FRONTEND_URL,
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true
-}))
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL, // React app URL
+    methods: ["GET", "POST", "PUT", "DELETE"], // Specify allowed HTTP methods
+    credentials: true, // allow cookies
+  })
+);
 
 app.all("/api/auth/*splat", toNodeHandler(auth));
 
 app.use(express.json());
-app.use(securityMiddleware);
-app.use('/api/subjects', subjectsRouter);
-app.use('/api/users', usersRouter);
-app.use('/api/classes', classesRouter);
 
-app.get('/', (req, res) => {
-  res.send('Hello, welcome to the Classroom API!');
+// app.use(securityMiddleware);
+
+app.use("/api/subjects", subjectsRouter);
+app.use("/api/users", usersRouter);
+app.use("/api/classes", classesRouter);
+
+app.get("/", (req, res) => {
+  res.send("Backend server is running!");
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
+  console.log(`Server running at http://localhost:${PORT}`);
 });
